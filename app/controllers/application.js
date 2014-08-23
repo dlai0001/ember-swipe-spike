f30246c59c4adbd9dc7e588cbcae3abb59cf4c8e
David Lai <me@dlai0001.com>
2014-08-23 10:34:20 -0700

    added animation effects

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,11 +3,14 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   previousPageTarget:null,
   nextPageTarget: null,
+  lastSwipeDirection: "right",
 
   actions: {
     nextPage: function() {
       console.log("next page action", this.get('nextPageTarget'));
+
       if(this.get('nextPageTarget')) {
+        this.set('lastSwipeDirection', "right");
         this.transitionToRoute(this.get('nextPageTarget'));
       }
 
@@ -15,6 +18,7 @@ export default Ember.Controller.extend({
     previousPage: function() {
       console.log("previous page action", this.get('previousPageTarget'));
       if(this.get('previousPageTarget')) {
+        this.set('lastSwipeDirection', "left");
         this.transitionToRoute(this.get('previousPageTarget'));
       }
     }
--- app/views/basemobileview.js
+++ app/views/basemobileview.js
@@ -2,14 +2,31 @@ import Ember from 'ember';
 
 export default Ember.View.extend({
   willAnimateIn : function () {
-    this.$().css("opacity", 0);
+    this.$().css("visibility", "hidden");
   },
 
   animateIn : function (done) {
-    this.$().fadeTo(500, 1, done);
+//    this.$().animate({
+//      left:"0em"
+//    }, 400, done);
+
+    var lastSwipeDir =this.get('controller').controllerFor('application').get("lastSwipeDirection");
+    console.log("last swipe direction", lastSwipeDir);
+    if(lastSwipeDir === "right") {
+      this.$().addClass('slideRight');
+    } else {
+      this.$().addClass('slideLeft');
+    }
+
+
+    this.$().css("visiblity", "visible");
+    done();
   },
 
   animateOut : function (done) {
-    this.$().fadeTo(500, 0, done);
+//    this.$().animate({
+//      left:"100%"
+//    }, 400, done);
+    done();
   }
 });
